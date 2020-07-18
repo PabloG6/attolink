791c7442c25876e32ef100d96b6272b502388f3b
Pablo Grant <grantpablo4@gmail.com>
2020-07-18 18:42:58 -0500

    attempting to fix permissions controller

--- lib/atto_link_web/controllers/permissions_controller.ex
+++ lib/atto_link_web/controllers/permissions_controller.ex
@@ -52,15 +52,19 @@ defmodule AttoLinkWeb.PermissionsController do
 
   def update(conn, %{"permissions" => permissions_params}) do
     %AttoLink.Accounts.User{id: id} = AttoLink.Auth.Guardian.Plug.current_resource(conn)
-    {:ok, permissions} = Security.get_permissions_by!(user_id: id)
-
-    with {:ok, %Permissions{} = permissions} <-
-           Security.update_permissions(
-             permissions,
-             permissions_params |> Enum.into(%{"user_id" => id})
-           ) do
-      render(conn, "show.json", permissions: permissions)
+    case Security.get_permissions_by!(user_id: id) do
+      {:ok, permissions} ->
+        with {:ok, %Permissions{} = permissions} <-
+          Security.update_permissions(
+            permissions,
+            permissions_params |> Enum.into(%{"user_id" => id})
+          ) do
+     render(conn, "show.json", permissions: permissions)
     end
+      {:error, :no_permissions} ->
+        Security.create_permissions(permissions_params |> Enum.into(%{"user_id" => id}))
+    end
+
   end
 
   def delete(conn, %{"id" => id}) do
