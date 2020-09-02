4cf9a943a654cad0b64ad1f1bb4f8da76aacf39b
Pablo Grant <grantpablo4@gmail.com>
2020-09-02 18:23:06 -0500

    added cors

--- lib/plug/authenticate_api.ex
+++ lib/plug/authenticate_api.ex
@@ -1,7 +1,8 @@
 defmodule AttoLink.Auth.API do
   import Plug.Conn
   alias AttoLink.Accounts
-
+  @admin_secret "ubPAbYOr2rNWgRDHLaac4aad+M5boGdMAcky5x3kEWA6lmsRNhBKSwmP+UKHHIps"
+  @admin_api_key  "attolink-api-key"
   @spec init(Keyword.t()) :: Keyword.t()
   def init(options), do: options
 
@@ -9,6 +10,19 @@ defmodule AttoLink.Auth.API do
   def call(conn, _opts) do
     conn
     |> verify_user()
+
+  end
+
+  defp verify_admin(%Plug.Conn{} = conn) do
+    with [key | _] <- get_req_header(conn, @admin_api_key),
+         true <- key == @admin_secret do
+
+      {:ok, :admin}
+    else
+      _ ->
+        {:error, :not_admin}
+
+    end
   end
 
   defp verify_user(%Plug.Conn{} = conn) do
@@ -79,7 +93,7 @@ defmodule AttoLink.Auth.API do
   end
 
   defp fetch_key(%Plug.Conn{} = conn) do
-    case get_req_header(conn, "apikey") do
+    case get_req_header(conn, "attolink-api-key") do
       [key | _tail] ->
         key
 
@@ -88,8 +102,10 @@ defmodule AttoLink.Auth.API do
     end
   end
 
+  defp
+
   @spec current_user(conn :: Plug.Conn.t()) :: {:error, :no_user} | {:ok, any} | {:error, :no_key}
-  def current_user(conn) do
+  def current_user(%Plug.Conn{} = conn) do
     key = fetch_key(conn)
     Accounts.get_user_by_api_key(key)
   end
