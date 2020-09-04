0d52433b8ba4189938d9e0ad5a014fccc01f4219
Pablo Grant <grantpablo4@gmail.com>
2020-09-03 23:26:50 -0500

    fixed cors to accept endpoints from mix environment

--- lib/plug/cors.ex
+++ lib/plug/cors.ex
@@ -1,19 +1,19 @@
 defmodule AttoLink.Cors do
   use Corsica.Router,
-    origins: [~r{^https?://(.*\.?)teenielink\.dev$}, "http://localhost"],
+    origins: [~r{^https?://(.*\.?)teenielink\.dev$}, "http://localhost:4200"],
     allow_credentials: true,
     allow_headers: :all,
     allow_methods: :all,
     max_age: 2000
 
-  resource "v1/*",
-    origins: "*",
+  resource "/*",
+    origins: [~r{^https?://(.*\.?)teenielink\.dev}, "http://localhost:4200"],
     allow_headers: :all,
     allow_methods: :all,
     allow_credentials: true
 
-  resource "/*",
-    origins: [~r{^https?://(.*\.?)teenielink\.dev}],
+  resource "v1/*",
+    origins: "*",
     allow_headers: :all,
     allow_methods: :all,
     allow_credentials: true
